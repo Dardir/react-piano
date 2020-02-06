6ba49ee2af36b172e78d5d6feb637ec33bede162
Mahmoud Dardir <eng.mahmoud.dardir@gmail.com>
2020-02-07 00:49:13 +0200

    Implementing Piano Feed view

--- src/components/PianoFeeder.js
+++ src/components/PianoFeeder.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { usePianoStore } from '../states/PianoStoreProvider'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMusic, faPlay } from '@fortawesome/free-solid-svg-icons'
+import Media from 'react-bootstrap/Media'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-const PianoFeeder = ()=>{
-  return(
-    <div>
-      PianoFeeder
-    </div>
-  )
-}
+const PianoFeeder = () => {
+    const { dispatch } = usePianoStore();
+    return (
+        <Media>
+            <FontAwesomeIcon icon={faMusic} size="lg" pull="left" />
+            <Media.Body>
 
+                <h5>Compose Your Music</h5>
+                <Form>
+                    <Form.Group controlId="formBasicEmail" style={{width: "310%"}}>
+                        <Form.Label>Music Note</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+                        <Form.Text className="text-muted">
+                            Please enter music letters as shown on the piano and hit Play
+                        </Form.Text>
+                    </Form.Group>
+                    <Button variant="outline-danger" type="submit">
+                        <FontAwesomeIcon icon={faPlay} size="lg" pull="left" /> Play
+                    </Button>
+                </Form>
+            </Media.Body>
+        </Media>
+            )
+          }
+          
 export default PianoFeeder;
