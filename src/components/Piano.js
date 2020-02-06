fddf17ac471044073bddd108188eb2fd83fcb917
Mahmoud Dardir <dardir@Mahmouds-MacBook-Pro.local>
2020-02-06 21:59:47 +0200

    Adding letters to Piano

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -4,18 +4,18 @@ import '../styles/Piano.css'
 const Piano = () => {
     return (
         <ul className="set">
-            <li className="white b"></li>
+            <li className="white b" value='C'>C</li>
             <li className="black as"></li>
-            <li className="white a"></li>
+            <li className="white a" value='D'>D</li>
             <li className="black gs"></li>
-            <li className="white g"></li>
+            <li className="white g" value='E'>E</li>
             <li className="black fs"></li>
-            <li className="white f"></li>
-            <li className="white e"></li>
+            <li className="white f" value='F'>F</li>
+            <li className="white e" value='G'>G</li>
             <li className="black ds"></li>
-            <li className="white d"></li>
+            <li className="white d" value='A'>A</li>
             <li className="black cs"></li>
-            <li className="white c"></li>
+            <li className="white c" value='B'>B</li>
         </ul>
     )
 }
