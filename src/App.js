1f43476d72637315a1927f31ca057e9674446480
Mahmoud Dardir <eng.mahmoud.dardir@gmail.com>
2020-02-07 11:23:39 +0200

    Implementing add piano mechanism

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import PianoApp from './components/PianoApp'
 import Container from 'react-bootstrap/Container'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import Button from 'react-bootstrap/Button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const App = ()=>{
-  return(
+const App = () => {
+  const [pianos, setPianos] = useState([<PianoApp/>]);
+  return (
     <Container>
-      <PianoApp/>
+      <Button variant="outline-primary" type="submit" onClick={() => setPianos([...pianos, <PianoApp/>])}>
+        <FontAwesomeIcon icon={faPlus} size="lg" pull="left" /> Add Piano
+      </Button>
+
+      {pianos}
     </Container>
   )
 }
